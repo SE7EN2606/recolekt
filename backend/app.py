c0cb93fe3b08a79e1c2f835f23e6f0245a5ee06a
SE7EN2606 <greg.busnel@icloud.com>
2025-10-14 23:21:46 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from tempfile import NamedTemporaryFile
-import shutil
 
 # ------------------------
 # Config
